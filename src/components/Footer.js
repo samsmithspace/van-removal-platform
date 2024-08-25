f9f6a6853bb306cd829017b284a9721fff62cdc2
samsmithspace <1286287511@qq.com>
2024-08-25 10:51:20 +0100

    homepage update 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Footer.css'; // Add relevant CSS for styling
-
+import xLogo from '../assets/images/xlogo.svg';
+import waLogo from '../assets/images/Digital_Glyph_Green.svg';
+import igLogo from '../assets/images/Instagram_Glyph_White.svg';
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-content">
-                <p>Contact us: +123 456 7890 | eremovals.uk</p>
+                <p>Contact us: 07917784728 | eremovals.uk</p>
                 <div className="social-icons">
-                    <img src="path/to/facebook-icon.svg" alt="Facebook" />
-                    <img src="path/to/twitter-icon.svg" alt="Twitter" />
-                    <img src="path/to/instagram-icon.svg" alt="Instagram" />
+
+                    <img src={xLogo} alt="X" />
+                    <img src={igLogo} alt="Instagram" />
+                    <img src={waLogo} alt="WhatsApp" />
+
                 </div>
             </div>
         </footer>
